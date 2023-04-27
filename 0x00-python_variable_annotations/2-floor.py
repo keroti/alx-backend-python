9f801cb42074e3e0264fbb17023999498fb91f3d
Peter Keroti <59761453+keroti@users.noreply.github.com>
2023-04-27 23:31:25 +0300

    Basic annotation - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import math
-
+""" Contains a function that calculates the floor of a number. """
 
 def floor(n: float) -> int:
+    """Returns the floor of a float n"""
     return math.floor(n)
