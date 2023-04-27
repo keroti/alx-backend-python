57234aaed64cf81d227030f119a6864d1dd68c35
Peter Keroti <59761453+keroti@users.noreply.github.com>
2023-04-27 23:29:11 +0300

    Update 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import math
-""" FUNCTION TO CALCULATE THE FLOOR A FLOAT """
+
+
 
 def floor(n: float) -> int:
-    """ returns floor of a float n."""
     return math.floor(n)
