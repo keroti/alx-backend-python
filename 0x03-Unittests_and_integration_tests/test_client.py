0ab6ca56630568e44ee6b777023a9000f186e5b8
keroti <peterkeroti55@gmail.com>
2023-05-22 18:31:24 +0300

    Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,15 +8,16 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
+    """ Test that json can be got """
 
     @parameterized.expand([
         ("google", {"google": True}),
         ("abc", {"abc": True})
     ])
     @patch('client.get_json')
-    def test_org(self, org_name, expected, patch):
-
-        patch.return_value =        expected
-        x = GithubOrgClient(org_name)
-        self.assertEqual(x.org_name, expected)
-        patch.assert_called_once_with("https://api.github.com/orgs/"+org_name)
+    def test_org(self, org, expected, get_patch):
+        """ Test the org of the client """
+        get_patch.return_value = expected
+        x = GithubOrgClient(org)
+        self.assertEqual(x.org, expected)
+        get_patch.assert_called_once_with("https://api.github.com/orgs/"+org)
