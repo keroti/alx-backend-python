59b99303abdec41b29c1b30001911f2ebb207fda
keroti <peterkeroti55@gmail.com>
2023-05-22 18:18:12 +0300

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -84,12 +84,10 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        test_instance = TestClass()
-
-        with patch.object(TestClass, 'a_method') as mock_a_method:
-            mock_a_method.return_value = 42
-            result1 = test_instance.a_property()
-            result2 = test_instance.a_property()
-            mock_a_method.assert_called_once()
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
+        with patch.object(TestClass, 'a_method', return_value=42) as patched:
+            test_class = TestClass()
+            return1 = test_class.a_property
+            return2 = test_class.a_property
+            self.assertEqual(return1, 42)
+            self.assertEqual(return2, 42)
+            patched.assert_called_once()
